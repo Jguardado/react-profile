3b70a8f151924e9331a510b0033bb55eddbca93f
Juan <jguardado85@gmail.com>
2017-08-14 09:02:05 -0700

    building games fro each framework

--- src/components/games.jsx
+++ src/components/games.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import CodeMirror from 'react-codemirror';
+import Example from './example.jsx';
+
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/ruby/ruby');
+
+const DemoNode = () => (
+  <div className="demo_node">
+    this would be a demo Node
+    <CodeMirror
+      ref="editor"
+      options={{
+        mode: 'javascript',
+      }}
+    />
+  </div>
+);
+
+const DemoReact = () => (
+  <div className="demo_react">
+    This is React
+    <CodeMirror
+      ref="editor"
+      options={{
+        mode: 'javascript',
+      }}
+    />
+  </div>
+);
+
+const DemoRuby = () => (
+  <div className="demo_ruby">
+    This would be ruby
+    <CodeMirror />
+  </div>
+);
 
 const DemoRedux = () => (
-  <div>
-      Lets play with Redux
+  <div className="demo_redux">
+    Lets play with Redux
+    <CodeMirror
+      ref="editor"
+      options={{
+        mode: 'javascript',
+      }}
+    />
   </div>
 );
 
@@ -11,8 +53,13 @@ const Games = () => (
     <div>
       Basic Games
     </div>
-    <DemoRedux />
+    <Example />
   </div>
 );
 
 export default Games;
+
+// <DemoReact />
+// <DemoRuby />
+// <DemoRedux />
+// <DemoNode />
