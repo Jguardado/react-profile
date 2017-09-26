74ee838fd5243c08542f2664ffa9fed2abb3326a
Juan <jguardado85@gmail.com>
2017-09-25 18:19:24 -0700

    working on bringing in andt

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Menu, Icon } from 'antd';
+
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import {
@@ -13,51 +15,95 @@ import {
   fetchPanelInfo,
 } from '../actions/info-panel-actions';
 
-const Header = ({
-  fetchBlogs,
-  fetchSummaries,
-  blogImages,
-  miniImages,
-  fetchCarousel,
-  fetchPanels,
-}) => (
-  <div className="header_container">
-    <div className="header_title">Profile of Juan Guardado</div>
-    <div>
-      <div className="header_span">Just a site to have fun and play with</div>
-      <div className="header_button_container">
-        <Link
-          onClick={() => {
-            fetchCarousel();
-            fetchPanels();
-          }}
-          className="header_button_link"
-          to="/"
-        >
-          <div className="header_button">
-            Home
+//
+// const SubMenu = Menu.SubMenu;
+// const MenuItemGroup = Menu.ItemGroup;
+
+// <Menu.Item key="mail">
+//        <Icon type="mail" />Navigation One
+//      </Menu.Item>
+
+class NavigationComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 'mail',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
+
+  render() {
+    const {
+      fetchBlogs,
+      fetchSummaries,
+      blogImages,
+      miniImages,
+      fetchCarousel,
+      fetchPanels,
+    } = this.props;
+
+    return (
+      <Menu
+        onClick={this.handleClick}
+        selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <Menu.Item key="carousel">
+          <div>
+            <Icon type="compass" />
+            <Link
+              onClick={() => {
+                fetchCarousel();
+                fetchPanels();
+              }}
+              to="/"
+            >
+                Home
+            </Link>
           </div>
-        </Link>
-        <Link
-          className="header_button_link"
-          to="/blog"
-          onClick={() => {
-            fetchBlogs();
-          }}
-        >
-          <div className="header_button">
-            Blog
+        </Menu.Item>
+        <Menu.Item key="blog">
+          <div>
+            <Icon type="coffee" />
+            <Link
+              to="/blog"
+              onClick={() => {
+                fetchBlogs();
+              }}
+            >
+                Blog
+            </Link>
           </div>
-        </Link>
-        <Link className="header_button_link" to="/games">
-          <div className="header_button">
-            Games
+        </Menu.Item>
+        <Menu.Item key="games">
+          <div>
+            <Icon type="fork" />
+            <Link to="/games">
+                Games
+            </Link>
           </div>
-        </Link>
-      </div>
-    </div>
-  </div>
-);
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+// NOTE: Wrapping info
+// <div className="header_container">
+//   <div className="header_title">Profile of Juan Guardado</div>
+//   <div>
+//     <div className="header_span">Just a site to have fun and play with</div>
+//
+//   </div>
+// </div>
 
 // const mapState = state => ({
 //   fetchSummaries: () => fetchBlogSummaries(state),
@@ -71,4 +117,4 @@ const mapDispatch = dispatch => ({
   fetchPanels: () => fetchPanelInfo(dispatch),
 });
 
-export default connect(null, mapDispatch)(Header);
+export default connect(null, mapDispatch)(NavigationComp);
--- src/components/landing/image-carousel.jsx
+++ src/components/landing/image-carousel.jsx
@@ -1,41 +1,57 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
+import { Pagination } from 'antd';
 import uuid from 'uuid';
 
-const ImageCarousel = ({ images, setImage, selectedImage, setInfoPanel }) =>
-  // console.log('props in Carousel: ', selectedImage);
-  (
-    <div>
-      <div
-        className="image_carousel_title"
-      >
-          Image Carousel
+class ImageCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 3,
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(page) {
+    console.log(page);
+    this.setState({
+      current: page,
+    });
+  }
+
+  render() {
+    const { images, setImage, selectedImage, setInfoPanel } = this.props;
+    console.log('props in Carousel: ', images.length);
+    return (
+      <div>
+        <div className="image_carousel_pic_box">
+          {
+            images.map((image, i) => (
+              <li
+                key={uuid()}
+                className="carousel_dots"
+                onClick={() => {
+                  setImage(image);
+                  setInfoPanel(i);
+                }}
+              />
+            ))
+          }
+        </div>
+        <Pagination current={this.state.current} onChange={this.onChange} total={50} />
+        <div
+          className="image_carousel_selected_container"
+        >
+          <img
+            src={selectedImage.image}
+            alt="Select to one from below"
+            className="image_carousel_selected"
+          />
+        </div>
       </div>
-      <div
-        className="image_carousel_selected_container"
-      >
-        <img
-          src={selectedImage.image}
-          alt="Select to one from below"
-          className="image_carousel_selected"
-        />
-      </div>
-      <div className="image_carousel_pic_box">
-        {
-          images.map((image, i) => (
-            <li
-              key={uuid()}
-              className="carousel_dots"
-              onClick={() => {
-                setImage(image);
-                setInfoPanel(i);
-              }}
-            />
-          ))
-        }
-      </div>
-    </div>
-  )
-;
+    );
+  }
+}
 
 
 ImageCarousel.propTypes = {
